2c95a5f77ec357b2904ee06211610c646e8bc9ea
Rahul19 <68568128+RahulUbale@users.noreply.github.com>
2023-10-19 07:34:04 -0400

    Update index.py

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ api_key = st.text_input('OpenAI API Key:', type='password')
 
 st.header('Your Personal chat bot - Donald!')
 
-question_for_donald = st.text_input('Question for Donald')
+question_for_donald = st.text_input('Question for ')
 
 cohere = CoHere(api_key)
 
 if st.button('Answer'):
-    st.write(cohere.cohere(question_for_donald))
+    st.write(cohere.cohere(question_for_))
